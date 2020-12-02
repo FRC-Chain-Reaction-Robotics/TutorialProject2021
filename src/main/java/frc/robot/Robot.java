ef476a862fe4a6eec913e439ffbcecaa2e4aef77
Joshua Purushothaman <52509691+joshuathooyavan@users.noreply.github.com>
2020-12-02 11:54:32 -0600

    Cleaned out files, here's a mostly empty project

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,46 +5,15 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-// Names:
-/*
-  Jacob Lewis
-  Zane Badgett
-  Joshua P
-  Arrio Gonsalves
-  Zayaan Rahman
-  Neeka Lin
-  Gabriel Rivera
-  Fernando Tovar
-  Matthew Metta
-*/
-
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.*;
-
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 
-public class Robot extends TimedRobot {
-  private static PWMVictorSPX lf = new PWMVictorSPX(Constants.LF_MOTOR_ID);
-  private static PWMVictorSPX lb = new PWMVictorSPX(Constants.LB_MOTOR_ID);
-  private static PWMVictorSPX rf = new PWMVictorSPX(Constants.RF_MOTOR_ID);
-  private static PWMVictorSPX rb = new PWMVictorSPX(Constants.RB_MOTOR_ID);
-  
-  private static SpeedControllerGroup leftSide = new SpeedControllerGroup(lf, lb);
-  private static SpeedControllerGroup rightSide = new SpeedControllerGroup(rf, rb);
+public class Robot extends TimedRobot
+{
+	@Override
+	public void teleopPeriodic()
+	{
 
-  DifferentialDrive dt = new DifferentialDrive(leftSide, rightSide);
-  XboxController driverController = new XboxController(0);
-
-  @Override
-  public void teleopPeriodic()
-  {
-    dt.arcadeDrive(driverController.getY(Hand.kLeft), driverController.getX(Hand.kRight));
-  }
+	}
 }
